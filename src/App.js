bb9abe806fa40fdfef0a4905533e38a3f3b7bd21
daisuke-ito <daisuke_ito@012grp.co.jp>
2023-02-03 15:20:39 +0900

    form追加

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      email,
+      password,
+    });
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" value={email} onChange={handleChangeEmail} />
+        </div>
+        <div>
+          <label htmlFor="password">パスワード</label>
+          <input
+            id="password"
+            name="password"
+            value={password}
+            onChange={handleChangePassword}
+            type="password"
+          />
+        </div>
+        <div>
+          <button type="submit">ログイン</button>
+        </div>
+      </form>
     </div>
   );
 }
